547384e9769b26f4048b0f3fcd46ca3f3b4cbd8d
桃乐丝 <71840053+ohohua@users.noreply.github.com>
2024-09-27 16:04:58 +0800

    🐞 fix:修改路由bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { close, start } from '@/utils/nprogress'
-import { errorRoute, routes } from './routes'
+import { createRouter, createWebHistory } from "vue-router";
+import { close, start } from "@/utils/nprogress";
+import { errorRoute, routes } from "./routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: '/login',
+      path: "/",
+      redirect: "/login",
     },
     {
-      path: '/login',
-      name: 'login',
-      redirect: '/login',
+      path: "/login",
+      name: "login",
+      redirect: "/login",
       children: [
         {
-          path: '/login',
-          name: 'loginPwd',
-          component: () => import('@/views/login/loginPwd.vue'),
-          meta: { title: '密码登录' },
+          path: "/login",
+          name: "loginPwd",
+          component: () => import("@/views/login/loginPwd.vue"),
+          meta: { title: "密码登录" },
         },
       ],
     },
@@ -26,36 +26,30 @@ const router = createRouter({
     ...errorRoute,
     // 后台业务
     {
-      path: '/admin',
-      name: 'admin',
-      redirect: '/admin/home', //重定向至后台首页
-      component: () => import('@/views/admin/admin.vue'),
+      path: "/admin",
+      name: "admin",
+      redirect: "/admin/home", //重定向至后台首页
+      component: () => import("@/views/admin/admin.vue"),
       children: [...routes],
     },
   ],
-})
+});
 
 /* 全局前置守卫 */
 router.beforeEach(async (to) => {
-  start() // 开启进度条
-  const isLogin = !!localStorage.getItem('token')
-  if (isLogin) {
-    if (to.meta.role === 'admin')
-      if (localStorage.getItem('scope') === 'admin') {
-        return true
-      } else {
-        return { name: 'Forbidden' }
-      }
-    return true
-  } else {
-    return { name: 'login' }
+  start(); // 开启进度条
+
+  const isLogin = !!localStorage.getItem("token");
+  if (to.path !== "/login" && !isLogin) {
+    return { name: "login" };
   }
-})
+  return true;
+});
 
 /* 全局后置守卫 */
 router.afterEach((to) => {
-  close() //关闭进度条
-  document.title = to.meta.title as string
-})
+  close(); //关闭进度条
+  document.title = to.meta.title as string;
+});
 
-export default router
+export default router;
